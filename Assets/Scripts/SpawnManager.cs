202fc0f26c7bea395b241704467d3a04bd7cb22b
Kenton Renshaw <kentorenshaw@gmail.com>
2021-03-20 20:40:46 -0600

    ball spawn interval

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,10 +9,10 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] ballPrefabs;
     private float spawnRangeZ = 7.0f;
     private float startDelay = 2.0f;
-    private float spawnInterval = 1.5f;
 
     private void Start()
     {
+        float spawnInterval = Random.Range(3.0f, 5.0f);
         InvokeRepeating(nameof(SpawnRandomBall), startDelay, spawnInterval);
     }
     
@@ -21,5 +21,9 @@ public class SpawnManager : MonoBehaviour
         Vector3 spawnPos = new Vector3(0, 11, 8 + Random.Range(-spawnRangeZ, spawnRangeZ));
         int ballIndex = Random.Range(0, ballPrefabs.Length);
         Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+        
+        float spawnInterval = Random.Range(3.0f, 5.0f);
+        CancelInvoke(nameof(SpawnRandomBall));
+        InvokeRepeating(nameof(SpawnRandomBall), startDelay, spawnInterval);
     }
 }
